346c8b389bae7c97c1e2e07f7f49a2e835ebb851
aikasi <32337856+aikasi@users.noreply.github.com>
2021-02-28 04:37:41 +0900

    feat: NesJS, Graphql 기본설정 및 기초

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,15 +1,45 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  @IsNumber()
+  id: number;
+
   @Field(() => String)
+  @Column()
+  @IsString()
+  @Length(5)
   name: string;
-  @Field(() => Boolean)
+
+  @Field(() => Boolean, { defaultValue: true }) //graphql
+  @Column({ default: true }) //database
+  @IsOptional() //dto
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
+  @IsString()
   address: string;
 
   @Field(() => String)
+  @Column()
+  @IsString()
   ownerName: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  categoryName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,15 +1,43 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
-  @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation(() => Boolean)
-  createRestaurant(@Args() createRestaurantdto: createRestaurantDto): boolean {
-    return true;
+  async createRestaurant(
+    @Args('input') createRestaurantdto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    console.log(createRestaurantdto);
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantdto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args() updateRestaurantDto: UpdateRestaurantDto,
+    // @Args('id') id: number,
+    // @Args('data') data: UpdateRestaurantDto,
+    //dto에 ArgsType을 넣어서 한개로 줄일 수 있음
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.updateRestaurant(updateRestaurantDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
