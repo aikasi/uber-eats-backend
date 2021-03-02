b8229d201a39de979d927b24deb10d88cc0aea55
aikasi <32337856+aikasi@users.noreply.github.com>
2021-03-03 06:19:32 +0900

    feat: User Module E2E Testing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -105,6 +105,7 @@ export class UsersService {
       if (email) {
         user.email = email;
         user.verified = false;
+        await this.verification.delete({ user: { id: user.id } });
         const verification = await this.verification.save(
           this.verification.create({ user }),
         );
